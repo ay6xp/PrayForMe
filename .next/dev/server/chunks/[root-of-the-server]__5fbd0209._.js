4ee06172faa891dff4e8ccae17553d9231868ab6
ay6xp123 <ay6xp123@gmail.com>
2025-12-30 15:30:50 -0500

    fixing route.ts to be nextjs 15 compatitble

--- .next/dev/server/chunks/[root-of-the-server]__5fbd0209._.js
+++ .next/dev/server/chunks/[root-of-the-server]__5fbd0209._.js
@@ -209,9 +209,9 @@ const schema = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$
 
 __turbopack_context__.s([
     "GET",
-    ()=>yoga,
+    ()=>GET,
     "POST",
-    ()=>yoga
+    ()=>POST
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$graphql$2d$yoga$2f$esm$2f$server$2e$js__$5b$app$2d$route$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/graphql-yoga/esm/server.js [app-route] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$app$2f$server$2f$schema$2e$ts__$5b$app$2d$route$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/app/server/schema.ts [app-route] (ecmascript)");
@@ -225,7 +225,16 @@ const yoga = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f
         Response
     }
 });
-;
+async function GET(request) {
+    return yoga.handleRequest(request, {
+        req: request
+    });
+}
+async function POST(request) {
+    return yoga.handleRequest(request, {
+        req: request
+    });
+}
 }),
 ];
 
--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,12 +1,19 @@
-import {NextRequest} from 'next/server';
-import {createYoga} from 'graphql-yoga';
-import {schema} from '../../server/schema';
+import { NextRequest } from 'next/server';
+import { createYoga } from 'graphql-yoga';
+import { schema } from '../../server/schema';
 
 const yoga = createYoga<{
-    req: NextRequest;}>({
-    schema,
-    graphqlEndpoint: '/api/graphql',
-    fetchAPI: {Request, Response},
-    });
+  req: NextRequest;
+}>({
+  schema,
+  graphqlEndpoint: '/api/graphql',
+  fetchAPI: { Request, Response },
+});
 
-export {yoga as GET, yoga as POST};
+export async function GET(request: NextRequest) {
+  return yoga.handleRequest(request, { req: request });
+}
+
+export async function POST(request: NextRequest) {
+  return yoga.handleRequest(request, { req: request });
+}
